921c799cc3aa3103b300b8bcad4bdb5b5353e39e
Jack Kelly <jack.kelly@bellroy.com>
2024-08-20 16:07:40 +1000

    Add pre-commit-hooks flake input, remove flake-compat

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   # Use 'nix flake show' to discover the structure of the output.
   # Multiple versions of compiler is supported.
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     haskell-ci = {
       url = "github:haskell-ci/haskell-ci";
       flake = false;
     };
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    pre-commit-hooks = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:cachix/pre-commit-hooks.nix";
     };
   };
 
