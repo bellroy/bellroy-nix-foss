0189ba8313f6b69e3bedcd68e563eb6074f9aa22
Jack Kelly <jack.kelly@bellroy.com>
2024-08-20 14:22:39 +1000

    Add haskell-ci flake input

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   # Use 'nix flake show' to discover the structure of the output.
   # Multiple versions of compiler is supported.
   inputs = {
+    haskell-ci = {
+      url = "github:haskell-ci/haskell-ci";
+      flake = false;
+    };
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
