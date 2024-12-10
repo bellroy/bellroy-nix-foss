9758ab8a249a695cc1a5fe686b6a66d5f50319d8
Michael Webb <michaelwebb76@gmail.com>
2024-12-10 20:58:20 +1100

    Remove Haskell CI

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
   # Multiple versions of compiler is supported.
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    haskell-ci = {
-      url = "github:haskell-ci/haskell-ci";
-      flake = false;
-    };
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     pre-commit-hooks = {
       inputs.nixpkgs.follows = "nixpkgs";
